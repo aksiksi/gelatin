6b4b23ea1a5d203d84973264301e71e6bffaa690
Assil Ksiksi <cyph0nik@gmail.com>
2021-09-08 21:36:32 +0000

    Completed System unit tests for Emby

--- api/emby/client_test.go
+++ api/emby/client_test.go
@@ -2,9 +2,11 @@ package emby
 
 import (
 	"encoding/json"
+	"fmt"
 	"io"
 	"net/http"
 	"net/http/httptest"
+	"os"
 	"testing"
 
 	"github.com/aksiksi/gelatin/api"
@@ -43,8 +45,9 @@ func TestEmbySystemEndpoints(t *testing.T) {
 
 	apiKey := api.NewApiKey("test123")
 
+	s.status = http.StatusOK
+
 	t.Run("SystemPing", func(t *testing.T) {
-		s.status = http.StatusOK
 		err := client.SystemPing()
 		if err != nil {
 			t.Errorf("failed to call ping endpoint")
@@ -52,55 +55,82 @@ func TestEmbySystemEndpoints(t *testing.T) {
 	})
 
 	t.Run("SystemInfoPublic", func(t *testing.T) {
-		s.status = http.StatusOK
-		wantResp := &EmbySystemInfoPublicResponse{
-			LocalAddress: "127.0.0.1",
-			Version:      "4.4.4",
-		}
-		bytes, _ := json.Marshal(wantResp)
-		s.resp = bytes
+		wantResp := []byte(`{
+			"ServerName": "abc",
+			"Version": "4.6.4.0",
+			"Id": "ec68c767780f485d9fd4b3d58594f5ff"
+		}`)
 
-		resp, err := client.SystemInfoPublic()
+		s.resp = wantResp
+
+		want := &EmbySystemInfoPublicResponse{}
+		json.Unmarshal(wantResp, want)
+
+		got, err := client.SystemInfoPublic()
 		if err != nil {
 			t.Errorf("failed to call SystemInfoPublic endpoint")
 		}
 
-		if diff := cmp.Diff(wantResp, resp); diff != "" {
+		if diff := cmp.Diff(want, got); diff != "" {
 			t.Errorf("+want,-got: %s", diff)
 		}
 	})
 
 	t.Run("GetVersion", func(t *testing.T) {
-		s.status = http.StatusOK
-		wantResp := &EmbySystemInfoPublicResponse{
-			LocalAddress: "127.0.0.1",
-			Version:      "4.4.4",
-		}
-		bytes, _ := json.Marshal(wantResp)
-		s.resp = bytes
+		wantResp := []byte(`{
+			"ServerName": "abc",
+			"Version": "4.6.4.0",
+			"Id": "ec68c767780f485d9fd4b3d58594f5ff"
+		}`)
+		s.resp = wantResp
+
+		want := &EmbySystemInfoPublicResponse{}
+		json.Unmarshal(wantResp, want)
 
 		version, err := client.GetVersion()
 		if err != nil {
-			t.Errorf("failed to call SystemInfoPublic endpoint")
+			t.Errorf("failed to call GetVersion")
 		}
 
-		if version != wantResp.Version {
-			t.Errorf("version mismatch")
+		if version != want.Version {
+			t.Errorf("version mismatch: want %q != got %q", want, version)
 		}
 	})
 
 	t.Run("SystemLogs", func(t *testing.T) {
-		s.status = http.StatusOK
 		wantResp := []byte("this is a log file")
 		s.resp = wantResp
 
 		logReader, err := client.SystemLogs(apiKey, "test")
 		if err != nil {
-			t.Errorf("failed to call SystemInfoPublic endpoint")
+			t.Errorf("failed to call SystemLogs")
 		}
 		logData, _ := io.ReadAll(logReader)
 
 		want, got := string(wantResp), string(logData)
+		if diff := cmp.Diff(want, got); diff != "" {
+			t.Errorf("+want,-got:%s", diff)
+		}
+	})
+
+	t.Run("SystemLogsQuery", func(t *testing.T) {
+		path := fmt.Sprintf("testdata/%s_valid.json", t.Name())
+		f, err := os.Open(path)
+		if err != nil {
+			t.Fatalf("failed to open test file: %s", path)
+		}
+		data, _ := io.ReadAll(f)
+
+		s.resp = data
+
+		want := &EmbySystemLogsQueryResponse{}
+		json.Unmarshal(data, want)
+
+		got, err := client.SystemLogsQuery(apiKey)
+		if err != nil {
+			t.Errorf("failed to call SystemLogsQuery")
+		}
+
 		if diff := cmp.Diff(want, got); diff != "" {
 			t.Errorf("+want,-got:%s", diff)
 		}
